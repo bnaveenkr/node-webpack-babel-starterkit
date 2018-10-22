05543c262c972cf5b31669bd0d1efad1f08a3c8b
Naveen Kumar <bolla.naveenkumar@gmail.com>
2018-10-22 14:11:46 +0530

    [FIX] Config overrides and env

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import nconf from 'nconf';
 
-const cfg = require('./config');
-nconf.defaults(cfg);
+const envOptions = {
+    lowerCase: true,
+    parseValues: true,
+    separator: '__',
+};
 
-const userConfig = path.join(__dirname, 'config.json');
+
+const defaults = nconf.file({ file: path.join(__dirname, 'config.json') });
+nconf.defaults(defaults.get());
+
+const userConfig = path.join(__dirname, '..', 'config.json');
 if (fs.existsSync(userConfig)) {
     nconf.overrides(nconf.file(userConfig).get());
 }
 
-nconf.argv();
+nconf.argv().env(envOptions);
 
 export default nconf;
